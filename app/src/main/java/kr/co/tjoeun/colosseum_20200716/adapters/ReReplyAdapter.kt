b10b65196d386f9e4e7575a3a2b2b5df9161d592
조장우 <jopuk2@gmail.com>
2020-07-27 20:43:01 +0900

    답글 어댑터 뼈대 작업진행

---
+++ app/src/main/java/kr/co/tjoeun/colosseum_20200716/adapters/ReReplyAdapter.kt
@@ -0,0 +1,41 @@
+package kr.co.tjoeun.colosseum_20200716.adapters
+
+import android.content.Context
+import android.content.Intent
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ArrayAdapter
+import android.widget.Button
+import android.widget.TextView
+import kotlinx.android.synthetic.main.activity_view_topic_detail.*
+import kr.co.tjoeun.colosseum_20200716.R
+import kr.co.tjoeun.colosseum_20200716.ViewReplyDetailActivity
+import kr.co.tjoeun.colosseum_20200716.datas.Reply
+import kr.co.tjoeun.colosseum_20200716.utils.TimeUtil
+import java.text.SimpleDateFormat
+
+class ReReplyAdapter(
+    val mContext:Context,
+    resId: Int,
+    val mList:List<Reply>) : ArrayAdapter<Reply>(mContext, resId, mList) {
+
+    val inf = LayoutInflater.from(mContext)
+
+    //    실제 목록을 뿌려줄 어댑터
+    lateinit var mReplyAdapter : ReReplyAdapter
+
+
+    override fun getView(position: Int, convertView: View?, parent: ViewGroup): View {
+        var tempRow = convertView
+
+        if (tempRow == null) {
+            tempRow = inf.inflate(R.layout.re_reply_list_item, null)
+        }
+
+        val row = tempRow!!
+
+        return row
+    }
+
+}
